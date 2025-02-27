18fbf6534ad910613ca4c8ad31eb9d3375d1dc89
Hanzo Alpha <hansen.makangiras@hotmail.com>
2025-02-28 03:02:53 +0800

    add test

--- src/Transactions/CloseTransaction.php
+++ src/Transactions/CloseTransaction.php
@@ -2,11 +2,13 @@
 
 namespace HanzoAlpha\LaravelTripay\Transactions;
 
+use HanzoAlpha\LaravelTripay\Exceptions\InvalidCredentialException;
 use HanzoAlpha\LaravelTripay\Exceptions\InvalidSignatureHashException;
 use HanzoAlpha\LaravelTripay\Requests\TripayClient;
-use HanzoAlpha\LaravelTripay\Transactions\Transaction;
+use HanzoAlpha\LaravelTripay\Signature;
 use HanzoAlpha\LaravelTripay\Validator\CreateCloseTransactionFormValidation;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Validator;
 
 class CloseTransaction implements Transaction
 {
@@ -30,8 +32,12 @@ class CloseTransaction implements Transaction
 
     /**
      * @inheritDoc
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \HanzoAlpha\LaravelTripay\Exceptions\InvalidSignatureHashException
+     * @throws \HanzoAlpha\LaravelTripay\Exceptions\TripayValidationException
+     * @throws \HanzoAlpha\LaravelTripay\Exceptions\InvalidCredentialException
      */
-    public function createTransaction(array $data): \HanzoAlpha\LaravelTripay\Transactions\Transaction
+    public function createTransaction(array $data): Transaction
     {
         $validated = CreateCloseTransactionFormValidation::validate($data);
 
@@ -52,15 +58,24 @@ class CloseTransaction implements Transaction
      */
     public function getResponse(): Collection
     {
-        // TODO: Implement getResponse() method.
+        return collect(json_decode($this->response, true));
     }
 
     /**
      * @inheritDoc
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function getDetailTransaction(string $refNumber): \HanzoAlpha\LaravelTripay\Transactions\Transaction
+    public function getDetailTransaction(string $refNumber): Transaction
     {
-        // TODO: Implement getDetailTransaction() method.
+        $validated = Validator::make([
+            'reference' => $refNumber
+        ], [
+            'reference' => 'required|string'
+        ])->validate();
+
+        $this->response = $this->httpClient->sendRequest('GET', 'transaction/detail', $validated);
+
+        return $this;
     }
 
     /**
@@ -68,6 +83,10 @@ class CloseTransaction implements Transaction
      */
     public function setSignatureHash(array $data): string
     {
-        // TODO: Implement setSignatureHash() method.
+        if (isset($data['merchant_ref']) && isset($data['amount'])) {
+            return $data['merchant_ref'].$data['amount'];
+        }
+
+        throw new InvalidCredentialException('gagal melakukan hash. data merchant_ref atau amount belum dikonfigurasi');
     }
 }
---
+++ tests/TripayTest.php
@@ -0,0 +1,230 @@
+<?php
+
+use HanzoAlpha\LaravelTripay\Exceptions\TripayValidationException;
+use HanzoAlpha\LaravelTripay\LaravelTripay;
+use HanzoAlpha\LaravelTripay\Requests\TripayClient;
+use HanzoAlpha\LaravelTripay\Signature;
+use Mockery\MockInterface;
+
+it('can test', function () {
+    expect(true)->toBeTrue();
+});
+
+it('init configuration', function () {
+    LaravelTripay::loadConfig(true, 'api_key', 'private_key', 'merchant_code');
+
+    $this->assertArrayHasKey('tripay_api_production', config('tripay'));
+    $this->assertArrayHasKey('tripay_api_key', config('tripay'));
+    $this->assertArrayHasKey('tripay_private_key', config('tripay'));
+
+    $this->assertTrue(config('tripay.tripay_api_production'));
+    $this->assertEquals('api_key', config('tripay.tripay_api_key'));
+    $this->assertEquals('private_key', config('tripay.tripay_private_key'));
+    $this->assertEquals('merchant_code', config('tripay.tripay_merchant_code'));
+});
+
+/**
+ * @throws \HanzoAlpha\LaravelTripay\Exceptions\InvalidTransactionException
+ * @throws \HanzoAlpha\LaravelTripay\Exceptions\InvalidSignatureHashException
+ */
+it('create open transaction', function () {
+    $data = [
+        'method' => 'BRIVA',
+        'merchant_ref' => 'INV12345',
+        'customer_name' => 'Nama Pelanggan',
+        'signature' => Signature::generate('BRIVAINV12345')
+    ];
+
+    // fake api production
+    config(['tripay.tripay_api_production' => true]);
+
+    /**
+     * @var \HanzoAlpha\LaravelTripay\Requests\TripayClient $fakeHttpClient
+     */
+    $fakeHttpClient = $this->mock(TripayClient::class, function (MockInterface $mock) {
+        return $mock->shouldReceive('sendRequest')
+            ->once()
+            ->andReturn(file_get_contents(__DIR__.'/mock/open_transaction/success.json'));
+    });
+
+    $tripay = new LaravelTripay($fakeHttpClient);
+    $result = $tripay->createTransaction($data, LaravelTripay::OPEN_TRANSACTION)->getResponse();
+
+    $this->assertTrue($result['success']);
+});
+
+/**
+ * @throws \HanzoAlpha\LaravelTripay\Exceptions\InvalidTransactionException
+ * @throws \HanzoAlpha\LaravelTripay\Exceptions\InvalidSignatureHashException
+ */
+it('create close transaction', function () {
+    $data = [
+        'method' => 'BRIVA',
+        'merchant_ref' => 'KODE INVOICE',
+        'amount' => 50000,
+        'customer_name' => 'Nama Pelanggan',
+        'customer_email' => 'emailpelanggan@domain.com',
+        'customer_phone' => '081234567890',
+        'order_items' => [
+            [
+                'sku' => 'FB-06',
+                'name' => 'Nama Produk 1',
+                'price' => 50000,
+                'quantity' => 1,
+                'product_url' => 'https://tokokamu.com/product/nama-produk-1',
+                'image_url' => 'https://tokokamu.com/product/nama-produk-1.jpg',
+            ]
+        ],
+        'return_url' => 'https://domainanda.com/redirect',
+        'expired_time' => (time() + (24 * 60 * 60)), // 24 jam
+        'signature' => Signature::generate('KODE INVOICE'. 50000)
+    ];
+
+    /**
+     * @var TripayClient $fakeHttpClient
+     */
+    $fakeHttpClient = $this->mock(TripayClient::class, function (MockInterface $mock) {
+        return $mock->shouldReceive('sendRequest')
+            ->once()
+            ->andReturn(file_get_contents(__DIR__.'/mock/close_transaction/success.json'));
+    });
+
+    $tripay = new LaravelTripay($fakeHttpClient);
+    $result = $tripay->createTransaction($data)->getResponse();
+
+    $this->assertTrue($result['success']);
+});
+
+/**
+ * @throws \HanzoAlpha\LaravelTripay\Exceptions\InvalidTransactionException
+ */
+it('get detail closed transaction', function () {
+    /**
+     * @var TripayClient $fakeHttpClient
+     */
+    $fakeHttpClient = $this->mock(TripayClient::class, function (MockInterface $mock) {
+        return $mock->shouldReceive('sendRequest')
+            ->once()
+            ->andReturn(file_get_contents(__DIR__.'/mock/close_transaction/detail_success.json'));
+    });
+
+    $tripay = new LaravelTripay($fakeHttpClient);
+    $result = $tripay->getDetailTransaction('T0001000000000000006')->getResponse();
+
+    $this->assertTrue($result['success']);
+    $this->assertEquals('T0001000000000000006', $result['data']['reference']);
+});
+
+it('get detail open transaction', function () {
+    $fakeHttpClient = $this->mock(TripayClient::class, function (MockInterface $mock) {
+        return $mock->shouldReceive('sendRequest')
+            ->once()
+            ->andReturn(file_get_contents(__DIR__.'/mock/open_transaction/detail_success.json'));
+    });
+
+    $tripay = new LaravelTripay($fakeHttpClient);
+    $result = $tripay->getDetailTransaction('T0001OP9376HnpS', LaravelTripay::OPEN_TRANSACTION)
+        ->getResponse();
+    $this->assertTrue($result['success']);
+    $this->assertEquals('T0001OP9376HnpS', $result['data']['uuid']);
+});
+
+it('get instruksi pembayaran', function () {
+    /**
+     * @var TripayClient $fakeHttpClient
+     */
+    $fakeHttpClient = $this->mock(TripayClient::class, function (MockInterface $mock) {
+        return $mock->shouldReceive('sendRequest')
+            ->once()
+            ->andReturn(file_get_contents(__DIR__.'/mock/instruksi_pembayaran/success.json'));
+    });
+
+    $tripay = new LaravelTripay($fakeHttpClient);
+
+    $result = $tripay->getInstruksiPembayaran('BRIVA');
+    // $result = $tripay->getInstruksiPembayaran('BRIVA', '264006510417648');
+    // $result = $tripay->getInstruksiPembayaran('BRIVA', '264006510417648', '50000');
+
+    $this->assertTrue($result['success']);
+});
+
+it('get channel pembayaran', function () {
+    /**
+     * @var TripayClient $fakeHttpClient
+     */
+    $fakeHttpClient = $this->mock(TripayClient::class, function (MockInterface $mock) {
+        return $mock->shouldReceive('sendRequest')
+            ->once()
+            ->andReturn(file_get_contents(__DIR__.'/mock/channel_pembayaran/success.json'));
+    });
+
+    $tripay = new LaravelTripay($fakeHttpClient);
+
+    $result = $tripay->getChannelPembayaran();
+    // $result = $tripay->getChannelPembayaran('PERMATAVA');
+    $this->assertTrue($result['success']);
+    $this->assertCount(14, $result['data']);
+});
+
+it('get biaya transaksi', function () {
+    /**
+     * @var TripayClient $fakeHttpClient
+     */
+    $fakeHttpClient = $this->mock(TripayClient::class, function (MockInterface $mock) {
+        return $mock->shouldReceive('sendRequest')
+            ->once()
+            ->andReturn(file_get_contents(__DIR__.'/mock/biaya_transaksi/success.json'));
+    });
+
+    $tripay = new LaravelTripay($fakeHttpClient);
+
+    $result = $tripay->getBiayaTransaksi(100000, 'BRIVA');
+    // $result = $tripay->getBiayaTransaksi(100000);
+    $this->assertTrue($result['success']);
+    $this->assertCount(1, $result['data']);
+});
+
+it('get daftar transaksi', function () {
+    /**
+     * @var TripayClient $fakeHttpClient
+     */
+    $fakeHttpClient = $this->mock(TripayClient::class, function (MockInterface $mock) {
+        return $mock->shouldReceive('sendRequest')
+            ->once()
+            ->andReturn(file_get_contents(__DIR__.'/mock/daftar_transaksi/success.json'));
+    });
+
+    $payload = [
+        'page' => 1,
+        'per_page' => 3,
+        'sort' => 'desc',
+    ];
+
+    $tripay = new LaravelTripay($fakeHttpClient);
+
+    $result = $tripay->getDaftarTransaksi($payload);
+
+    $this->assertTrue($result['success']);
+});
+
+it('generate and validate signature hash', function () {
+    $privateKey = config('tripay.tripay_private_key', 'VodDN-Gd63e-J4Vrf-XDggi-tLahm');
+    $merchantCode = config('tripay.tripay_merchant_code', 'T37858');
+    $merchantRef = 'INV55567';
+    $amount = 1500000;
+
+    $signature = hash_hmac('sha256', $merchantCode.$merchantRef.$amount, $privateKey);
+    $generatedSignature = Signature::generate($merchantRef.$amount);
+
+    $this->assertEquals($signature, $generatedSignature);
+    $this->assertTrue(Signature::validate($merchantRef.$amount, $signature));
+});
+
+it(/**
+ * @throws \HanzoAlpha\LaravelTripay\Exceptions\InvalidTransactionException
+ * @throws \HanzoAlpha\LaravelTripay\Exceptions\InvalidSignatureHashException
+ */ 'validation parameter exception', function () {
+    $this->expectException(TripayValidationException::class);
+
+    \HanzoAlpha\LaravelTripay\Facades\LaravelTripay::createTransaction([]);
+});
